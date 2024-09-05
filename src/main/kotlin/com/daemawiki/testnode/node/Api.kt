cf359f232937e59804aa484faa53381e35369af5
101 <tmddnjs001133@dsm.hs.kr>
2024-09-05 22:09:56 +0900

    update: change response type

--- src/main/kotlin/com/daemawiki/testnode/node/Api.kt
+++ src/main/kotlin/com/daemawiki/testnode/node/Api.kt
@@ -1,21 +1,23 @@
 package com.daemawiki.testnode.node
 
 import com.daemawiki.testnode.annotation.Api
+import org.bson.Document
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import reactor.core.publisher.Flux
 
 @Api
 class Api(
     private val subscriber: Subscriber
 ) {
     data class Response(
-        val document: Any?
+        val document: Document?
     )
 
     @GetMapping("/{collectionType}/{_id}")
     fun subscribe(
         @PathVariable collectionType: String,
         @PathVariable _id: String
-    ) = subscriber.execute(collectionType, _id)
+    ): Flux<Response> = subscriber.execute(collectionType, _id)
             .map { Response(it) }
 }
