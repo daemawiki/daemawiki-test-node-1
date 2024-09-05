638657c1ccad1b4cd1713eb6c0753e5978ebe24c
101 <tmddnjs001133@dsm.hs.kr>
2024-09-05 17:27:17 +0900

    init: init project

---
+++ src/main/kotlin/com/daemawiki/testnode/Application.kt
@@ -0,0 +1,11 @@
+package com.daemawiki.testnode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaemawikiTestNode1Application
+
+fun main(args: Array<String>) {
+    runApplication<DaemawikiTestNode1Application>(*args)
+}
---
+++ src/test/kotlin/com/daemawiki/testnode/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.daemawiki.testnode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DaemawikiTestNode1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
