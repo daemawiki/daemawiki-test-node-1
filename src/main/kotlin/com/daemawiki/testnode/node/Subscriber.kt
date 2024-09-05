50f5dd9350d6f88d3b966d5c65c8991ad9b17b3f
101 <tmddnjs001133@dsm.hs.kr>
2024-09-05 22:09:43 +0900

    update: apply custom mongoDSL

--- src/main/kotlin/com/daemawiki/testnode/node/Subscriber.kt
+++ src/main/kotlin/com/daemawiki/testnode/node/Subscriber.kt
@@ -1,11 +1,10 @@
 package com.daemawiki.testnode.node
 
+import com.daemawiki.testnode.mongo.ChangeStreamBuilder
 import com.mongodb.client.model.changestream.OperationType
 import org.bson.Document
 import org.bson.types.ObjectId
-import org.springframework.data.mongodb.core.ChangeStreamOptions
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
@@ -15,27 +14,21 @@ class Subscriber(
     private val mongoTemplate: ReactiveMongoTemplate
 ) {
 
-    fun execute(collectionType: String, _id: String): Flux<Any> {
-        return mongoTemplate.changeStream(
-            Collections.valueOf(collectionType).str,
-            ChangeStreamOptions.builder()
-                .filter(Aggregation.newAggregation(
-                    Aggregation.match(
-                        Criteria.where("operationType").`is`(OperationType.REPLACE)
-                            .orOperator(
-                                listOf(
-                                    Criteria.where("fullDocument._id").`is`(_id),
-                                    Criteria.where("documentKey._id").`is`(ObjectId(_id))
-                                )
-                            )
-                    )
-                ))
-                .build(),
-            Document::class.java
-        ).mapNotNull { it.body }
+    fun execute(collectionType: String, _id: String): Flux<Document> {
+        return mongoTemplate.changeStream {
+            collection(collectionType)
+            operationType(OperationType.REPLACE)
+            filterBy(Criteria.where("fullDocument._id").`is`(_id))
+            filterBy(Criteria.where("documentKey._id").`is`(ObjectId(_id)))
+        }
     }
+    
+}
 
-    internal enum class Collections(val str: String) {
-        ARTICLE("articles")
-    }
+internal fun ReactiveMongoTemplate.changeStream(init: ChangeStreamBuilder.() -> Unit): Flux<Document> {
+    val builder = ChangeStreamBuilder()
+        .apply(init)
+
+    return changeStream(builder.build(), Document::class.java)
+        .mapNotNull { it.body }
 }
