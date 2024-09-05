ec03737d85f9339cf8ff99fdab1804a445110280
101 <tmddnjs001133@dsm.hs.kr>
2024-09-05 17:28:34 +0900

    refactor: refactoring class name

--- src/main/kotlin/com/daemawiki/testnode/Application.kt
+++ src/main/kotlin/com/daemawiki/testnode/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DaemawikiTestNode1Application
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<DaemawikiTestNode1Application>(*args)
+    runApplication<Application>(*args)
 }
